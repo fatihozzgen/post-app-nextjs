b7bccdf3193a8574c0f27e1917344c4e0d982e9c
Fatih Özgen <97743229+fatihozzgen@users.noreply.github.com>
2023-06-22 22:03:21 +0300

    demo1.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
 import FormPost from './Form'
 
+async function getPosts() {
+  const baseURL = "https://post-app-nextjs-pi.vercel.app"; 
+  const res = await fetch(`${baseURL}/api/getPosts`, {
+    method: "GET",
+    cache: "no-cache"
+  });
+  return res.json()
+}
 
 export default async function Home() {
-
+  const data : { id: number; title: string} [] = await getPosts()
+  console.log(data);
+  
   return (
     <main className="flex justify-center pt-12 flex-col items-center ">
       
       <FormPost/>
-
-   
+<h1>deneme</h1>
+    
     </main>
   )
 }
