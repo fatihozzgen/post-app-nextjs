d177158b8e8a31079d870e0f4fad8e857f15a7f8
Fatih Özgen <97743229+fatihozzgen@users.noreply.github.com>
2023-06-22 20:23:53 +0300

    6.7

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import FormPost from './Form'
 
 async function getPosts() {
-  const res = await fetch(`post-app-nextjs-pi.vercel.app/api/getPosts`, {
+  const res = await fetch(`http://localhost:3000/api/getPosts`, {
     method: "GET",
     cache: "no-cache"
   });
