bcc915c97e4ad41abaffbd88a53b31df1e329249
Fatih Özgen <97743229+fatihozzgen@users.noreply.github.com>
2023-06-22 22:30:58 +0300

    demo1.5

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
+import axios from 'axios';
 import FormPost from './Form'
 
 async function getPosts() {
-  const baseURL = "https://post-app-nextjs-one.vercel.app"; 
-  const res = await fetch(`${baseURL}/api/getPosts`, {
-    method: "GET",
-    cache: "no-cache"
-  });
-  return res.json()
+  const baseURL = "http://localhost:3000"; 
+  const res = await axios.get(`${baseURL}/api/getPosts`);
+  return res.data
 }
 
 export default async function Home() {
@@ -17,8 +15,10 @@ export default async function Home() {
     <main className="flex justify-center pt-12 flex-col items-center ">
       
       <FormPost/>
-<h1>deneme</h1>
-    
+
+    {data.map(post => (
+      <h1 key={post.id} className='text-lg border-b border-b-3 border-[#884A39] w-1/2 text-left p-3'>{post.title}</h1>
+    ))}
     </main>
   )
 }
